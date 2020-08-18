0eccf864d56198d6e22b00a33da47943ae095091
Edward Smith <edward.m.smith80@gmail.com>
2020-08-18 03:51:47 +0000

    tail.js added with test cases

---
+++ tail.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const tail = function(arr) {
+  return arr.slice(1, arr.length);
+};
+
+// Test Case 1: Check the returned array elements
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+
+// Test Case 2: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+
+// Test Case 3: Check that an array with one element yields an empty array for its tail
+const emptyArray = tail(["Goodbye World"]);
+assertEqual(emptyArray.length, 0); // ensure we get back zero elements
+assertEqual(emptyArray[0], undefined); // ensure the array is empty
+
