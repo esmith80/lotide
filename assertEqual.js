6edbd718d9a6efbed445b68fe98e6a1aecc4f41a
11em <Edward@Edwards-iMac.local>
2020-08-17 21:01:09 -0600

    played around with template literals

--- assertEqual.js
+++ assertEqual.js
@@ -3,9 +3,9 @@ const assertEqual = function(actual, expected) {
 };
 
 // TEST CODE
+let word = 'Edward'
 assertEqual("Lighthouse Labs", "Bootcamp");
 assertEqual(1, 1);
 assertEqual(null, undefined);
 assertEqual(-1, null);
 assertEqual("Edward", "edward");
-assertEqual("Edward", "Edward");
