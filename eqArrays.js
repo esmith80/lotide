ca7dfb32da1398c4eb4fbc240f83597e41cd5050
Edward Smith <edward.m.smith80@gmail.com>
2020-08-19 01:40:43 +0000

    eqArrays.js comparison exercise added

---
+++ eqArrays.js
@@ -0,0 +1,20 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 4, 3]), true); // => should FAIL
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [1, 2, "3"]), true); // => should FAIL
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3, 4]), true); // => should FAIL
