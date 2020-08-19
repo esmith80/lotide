23858475791f212b2335bf76830f379f7947438e
Edward Smith <edward.m.smith80@gmail.com>
2020-08-19 14:45:12 +0000

    adding without.js with test cases included

---
+++ without.js
@@ -0,0 +1,43 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) return false;
+    }
+  } else {
+    return false;
+  }
+  return true;
+};
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅ Passed: The arrays are equal`);
+  } else {
+    console.log(`🛑 Failed: The arrays are NOT equal.`);
+  }
+};
+
+
+// Implement without which will return a subset of a given array, removing unwanted elements.
+const without = function(source, itemsToRemove) {
+  const newArray = [];
+  for (let i = 0; i < source.length; i++) {
+    if (!itemsToRemove.includes(source[i])) {
+      newArray.push(source[i]);
+    }
+  }
+  return newArray;
+};
+
+// TEST CASES
+
+assertArraysEqual(without([2, 9, 9, 1, 9, 3, 4, 1], [1, 9, 1, "hey"]), [2,3,4]); // should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2"]); // should PASS
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), [1, "2"]); // should FAIL
+assertArraysEqual(without(["1", "2", "3"], [1, 2, "3"]), ["1", "2", 0]); // should FAIL
+assertArraysEqual(without(["hey", "2", "3"], [2, "hey", "3"]), ["2"]); // should PASS
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
