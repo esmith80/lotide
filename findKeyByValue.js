4bd584377e62c94e710782caded3fc3aa24da448
Edward Smith <edward.m.smith80@gmail.com>
2020-08-20 01:16:22 +0000

    learned what Object.keys and Object.values do

---
+++ findKeyByValue.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected) {
+  (actual === expected) ? console.log(`✅ Assertion Passed: ${actual} === ${expected}`) : console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+};
+
+const findKeyByValue = function(obj, passedValue) {
+  //compare each object values to the value passed
+  for (let i = 0; i < Object.values(obj).length; i++) {
+    if (passedValue === Object.values(obj)[i]) return Object.keys(obj)[i];
+  }
+};
+
+// TEST
+const bestTVShowsByGenre = {
+  sciFi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
