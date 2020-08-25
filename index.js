b34298762f0197add17cfa3030ffe2ff03e9b58a
Edward Smith <edward.m.smith80@gmail.com>
2020-08-25 03:45:30 +0000

    added lotide object for export

---
+++ index.js
@@ -0,0 +1,33 @@
+// index.js
+const head                = require('./head');
+const tail                = require('./tail');
+const middle              = require('./middle');
+const assertArraysEqual   = require('./assertArraysEqual');
+const assertEqual         = require('./assertEqual');
+const assertObjectsEqual  = require('./assertObjectsEqual');
+const countOnly           = require('./countOnly');
+const eqArrays            = require('./eqArrays');
+const eqObjects           = require('./eqObjects');
+const findKey             = require('./findKey');
+const findKeyByValue      = require('./findKeyByValue');
+const map                 = require('./map');
+const takeUntil           = require('./takeUntil');
+const without             = require('./without');
+
+module.exports = {
+  head:               head,
+  tail:               tail,
+  middle:             middle,
+  assertArraysEqual:  assertArraysEqual,
+  assertEqual:        assertEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  assertObjectsEqual: assertObjectsEqual,
+  countOnly:          countOnly,
+  eqArrays:           eqArrays,
+  eqObjects:          eqObjects,
+  findKey:            findKey,
+  findKeyByValue:     findKeyByValue,
+  map:                map,
+  takeUntil:          takeUntil,
+  without:            without
+};
--- test/headTest.js
+++ test/headTest.js
@@ -1,3 +1,30 @@
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns number 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns the string '5' for ['5', 'foo', 'bar']", () => {
+    assert.strictEqual(head(['5', 'foo', 'bar']), '5'); 
+  });
+
+  it("should return one element for a single element array (e.g. [1] should return 1", () => {
+    assert.deepEqual(head([1]), 1); 
+  });
+
+  it("An empty array should yield undefined as its head", () => {
+    assert.deepEqual(head([]), undefined);
+  });
+
+});
+
+
+
+
+
+/*
 const head = require('../head');
 const assertEqual = require('../assertEqual');
 
@@ -8,3 +35,4 @@ assertEqual(head([5,6,7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), 3);
 assertEqual(head(['only item']), 'only item');
+*/
