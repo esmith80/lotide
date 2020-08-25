2053ff8e9b9f72f6614b830fbe74911e29923bf2
Edward Smith <edward.m.smith80@gmail.com>
2020-08-25 03:55:45 +0000

    refactored index.js export file for shorthand object property declaration

--- index.js
+++ index.js
@@ -15,19 +15,18 @@ const takeUntil           = require('./takeUntil');
 const without             = require('./without');
 
 module.exports = {
-  head:               head,
-  tail:               tail,
-  middle:             middle,
-  assertArraysEqual:  assertArraysEqual,
-  assertEqual:        assertEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  assertObjectsEqual: assertObjectsEqual,
-  countOnly:          countOnly,
-  eqArrays:           eqArrays,
-  eqObjects:          eqObjects,
-  findKey:            findKey,
-  findKeyByValue:     findKeyByValue,
-  map:                map,
-  takeUntil:          takeUntil,
-  without:            without
+  head,
+  tail,
+  middle,
+  assertArraysEqual,
+  assertEqual,
+  assertObjectsEqual,
+  countOnly,
+  eqArrays,
+  eqObjects,
+  findKey,
+  findKeyByValue,
+  map,
+  takeUntil,
+  without
 };
