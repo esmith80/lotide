d15a90f560426ebb2ba8c81fb1121dd389a78a09
Edward Smith <65724195+esmith80@users.noreply.github.com>
2020-08-24 19:00:20 -0600

    Update tailTest.js

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,19 +1,47 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
+const assert = require('chai').assert;
 
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
+describe("#tail", () => {
 
-// Test Case 2: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+  describe("Test Case 1: Check the returned array elements for length and equality", () => {
 
-// Test Case 3: Check that an array with one element yields an empty array for its tail
-const emptyArray = tail(["Goodbye World"]);
-assertEqual(emptyArray.length, 0); // ensure we get back zero elements
-assertEqual(emptyArray[0], undefined); // ensure the array is empty
+    const result = tail(["Hello", "Lighthouse", "Labs"]);
 
+    it("should return an array that is one shorter than was passed", () => {
+      assert.strictEqual(result.length, 2); // ensure we get back two elements
+    });
+
+    it("first element at 0 index should be the passed array 1 index ", () => {
+      assert.strictEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+    });
+
+    it("last element at last index should be the same as array that was passed ", () => {
+      assert.strictEqual(result[1], "Labs"); // ensure second element is "Labs"
+    });
+  });
+
+  describe("Test Case 2: Check the original array is not mutated)", () => {
+
+    const originalArr =  ["Hello", "Lighthouse", "Labs"];
+    const result = tail(originalArr);
+
+    it("original array should be the same length before and after the call to #tail", () => {
+      assert.strictEqual(result.length, originalArr.length - 1); // ensure original array is same length
+    });
+
+    it("original array should contain same values as before function was passed it", () => {
+      assert.strictEqual(originalArr[0], "Hello"); // ensure first element is "Lighthouse"
+      assert.strictEqual(originalArr[1], "Lighthouse"); // ensure second element is "Lighthouse"
+      assert.strictEqual(originalArr[2], "Labs"); // ensure third element is "Labs"
+    });
+  });
+
+  describe("Test Case 3: An array with one element should return an empty array/undefined)", () => {
+
+    const oneElementArr =  tail(["Goodbye World"]);
+    it("an array of one element yields an empty array for its tail", () => {
+      assert.deepEqual(oneElementArr, []); // ensure returned array is empty
+    });
+  });
+
+});
