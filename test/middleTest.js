c0e1e54047cf34c83c2d6b6b3d953c01d655f04c
Edward Smith <65724195+esmith80@users.noreply.github.com>
2020-08-24 18:59:42 -0600

    Update middleTest.js
    pulled this from my deleted local repo (was onscreen in VSCode and copied it)

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,13 +1,18 @@
-const assertArraysEqual = require('../assertEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-// PASS TESTS
-assertArraysEqual(middle([1,2,3]), [2]); //should PASS
-assertArraysEqual(middle([1,2,3,4]), [2,3]); //should PASS
-assertArraysEqual(middle([1,2]), []); //should PASS
-assertArraysEqual(middle([1]), []); //should PASS
-//FAIL TESTS
-assertArraysEqual(middle([1,2,3]), [1,2]); //should FAIL
-assertArraysEqual(middle([1,2,3,4]), [1,2,3]); //should FAIL
-assertArraysEqual(middle([1,2]), [2]); //should FAIL
-assertArraysEqual(middle([1]), [1]); //should FAIL
+describe("#middle", () => {
+
+  it("should return one value directly in the middle of an array for an odd number of elements", () => {
+    assert.deepEqual(middle([1,2,3]), [2]);
+  });
+
+  it("should return two values that constitute the middle for an even number of elements", () => {
+    assert.deepEqual(middle([1,2,3,4]), [2,3]); 
+  });
+
+  it("should return an empty array for an array that has only 1 or 2 elements", () => {
+    assert.deepEqual(middle([1,2]), []);
+    assert.deepEqual(middle([1]), []);
+  });
+});
